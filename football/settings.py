56c9f422901c6436b444a25836707b0286e8e799
Shad <farshadkalathingal@gmail.com>
2020-09-05 13:11:32 +0530

    First update

--- football/settings.py
+++ football/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2r82kb0$kw7_#+p)&r$8g^8s)+k0lf(60ismn0shad&y3&7+b1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['geany-football-live.herokuapp.com', '127.0.0.1']
 
